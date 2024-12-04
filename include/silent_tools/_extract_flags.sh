259297b74780ecf10c35198e06acdd332c466112
mubale-eit <mubale@eit.org>
2024-12-04 23:09:43 +0000

    added updated silent tools

--- include/silent_tools/_extract_flags.sh
+++ include/silent_tools/_extract_flags.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+unset SILENT_PARAMS
+unset SILENT_AT
+unset SILENT_FLAGS
+unset SILENT_FLAGS_NO_J
+
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]; do
@@ -8,20 +13,26 @@ while [[ $# -gt 0 ]]; do
   case $key in
     -p)
       SILENT_PARAMS="$SILENT_PARAMS -extra_res_fa $2"
+      SILENT_FLAGS="$SILENT_FLAGS -p $2"
+      SILENT_FLAGS_NO_J="$SILENT_FLAGS_NO_J -p $2"
       shift # past argument
       shift # past value
       ;;
     -j)
       SILENT_J="$2"
+      SILENT_FLAGS="$SILENT_FLAGS -j$2"
       shift # past argument
       shift # past value
       ;;
     -j*)
       SILENT_J="${1:2:${#1}-2}"
+      SILENT_FLAGS="$SILENT_FLAGS $1"
       shift # past value
       ;;
     @*)
       SILENT_AT="$SILENT_AT $1"
+      SILENT_FLAGS="$SILENT_FLAGS $1"
+      SILENT_FLAGS_NO_J="$SILENT_FLAGS_NO_J $1"
       shift # past value
       ;;
     *)    # unknown option
@@ -33,6 +44,8 @@ done
 
 export SILENT_PARAMS
 export SILENT_AT
+export SILENT_FLAGS
+export SILENT_FLAGS_NO_J
 
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
