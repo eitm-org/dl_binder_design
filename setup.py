d9cc81956d137735f4be419fd40dad62cce271c1
mubale-eit <mubale@eit.org>
2025-01-03 17:39:51 +0000

    added url

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
       name='dl_binder_design',
       version='0.0.1',
       description='alphafold2 initial guess (but no proteinMPNN)',
+      url = 'https://github.com/eitm-org/dl_binder_design',
       packages=find_packages(include=['af2_initial_guess', 'mpnn_fr']),
       install_requires=[
             'tqdm',
