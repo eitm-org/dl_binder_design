ddc40c4a6256e6fcf159bc36beb845e3ea4ea49c
mubale-eit <mubale@eit.org>
2025-01-03 17:36:09 +0000

    setup fixes

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+      name='dl_binder_design',
+      version='0.0.1',
+      description='alphafold2 initial guess (but no proteinMPNN)',
+      packages=find_packages(include=['af2_initial_guess', 'mpnn_fr']),
+      install_requires=[
+            'tqdm',
+            'numpy',
+            'torch',
+            'scipy',
+            'wandb',
+            'tensorboard',
+            'pytorch_lightning'
+      ],
+)
