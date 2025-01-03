ffdf53974d92eb97cebd9cb21cc7711eafc47d27
mubale-eit <mubale@eit.org>
2025-01-03 17:36:54 +0000

    setup fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
       name='dl_binder_design',
       version='0.0.1',
       description='alphafold2 initial guess (but no proteinMPNN)',
-      packages=find_packages(include=['af2_initial_guess', 'af2_initial_guess.*', 'mpnn_fr', 'mpnn_fr.*']),
+      packages=find_packages(include=['af2_initial_guess', 'mpnn_fr']),
       install_requires=[
             'tqdm',
             'numpy',
